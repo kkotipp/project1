1f242a096b3788acd8238069e93ab263039b0700
kkotipp <36917432+kkotipp@users.noreply.github.com>
2020-08-17 14:45:32 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,43 +1,3 @@
-
-Dashboard
-Issues
-Pull Requests
-Milestones
-Explore
-Notifications
- Create…
- Profile and Settings…
- docker-image / hostinfo
-Unwatch
-1
-Star
-0
-Fork
-0
-Code
-Issues
-0
-Pull Requests
-1
-Releases
-0
-Wiki
-Activity
-Settings
-No Description
-Manage Topics
- 66 Commits
- 2 Branches
- 88 KiB
- Branch: master 
-hostinfo/src/server.js
-40 lines
-941 B
-Raw
-Permalink
-Blame
-History
-  
 var express = require('express');
 var app = express();
 var audit = require('express-requests-logger')
@@ -77,4 +37,3 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at port %s !!!', port);
 });
 module.exports = server;
-Powered by Gitea Version: 1.12.3 Page: 9ms Template: 2ms JavaScript licenses API Website Go1.14.6
